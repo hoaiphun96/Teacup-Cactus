d07780e5babe9f4179f0af9dcf08d6cbd54023fb
Phuong <phnguyen@knox.edu>
2018-02-03 13:00:20 -0600

    Built for iOS
    I changed the build settings to iOS so this will be the official folder for TC iOS.
    
    I added background music and implemented mute/unmute button.
    
    TO DO:
    1. animated cactus and tea cup
    2. Picker scene
    3. Pedometer
    4. Crossbreed
    5. Credits scene
    6. Set goals feature in Settings Scene

---
+++ TC iOS/Assets/MuteUnmuteMusic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuteUnmuteMusic : MonoBehaviour {
+	bool Music = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void MuteUnmute() {
+		if (Music == true){
+			Music = false;
+			PlayMusic.Instance.gameObject.GetComponent<AudioSource> ().mute = true;
+		} else {
+			Music = true;
+			PlayMusic.Instance.gameObject.GetComponent<AudioSource> ().mute = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ TC iOS/Assets/changeScene.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void changeToScene() {
+		SceneManager.LoadScene ("Main Scene", LoadSceneMode.Additive);
+	}
+
+	public void changeToSettingsScene() {
+		SceneManager.LoadScene ("Settings Scene", LoadSceneMode.Additive);
+	}
+
+	public void changeToFlowerPickerScene() {
+		SceneManager.LoadScene ("Flower Picker Scene", LoadSceneMode.Additive);
+	}
+
+	public void changeToTeacupPickerScene() {
+		SceneManager.LoadScene ("Teacup Picker Scene", LoadSceneMode.Additive);
+	}
+
+	public void changeToTitleScene() {
+		SceneManager.LoadScene ("Title Scene", LoadSceneMode.Additive);
+	}
+}
