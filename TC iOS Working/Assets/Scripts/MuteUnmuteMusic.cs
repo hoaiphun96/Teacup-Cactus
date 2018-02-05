ac1d2b2a714ac7abb03396eb0f9ea1932460e2be
Mac User <macuser@CS-5891.local>
2018-02-04 20:16:08 -0600

    Re-added working unity project with the name TC iOS Working

---
+++ TC iOS Working/Assets/Scripts/MuteUnmuteMusic.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MuteUnmuteMusic : MonoBehaviour {
+	public bool Music = true;
+
+	public Sprite On;
+	public Sprite Off;
+	public Button MusicButton;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void MuteUnmute() {
+		if (Music == true){
+			Music = false;
+			PlayMusic.Instance.gameObject.GetComponent<AudioSource> ().mute = true;
+			MusicButton.image.sprite = Off;
+		} else {
+			Music = true;
+			PlayMusic.Instance.gameObject.GetComponent<AudioSource> ().mute = false;
+			MusicButton.image.sprite = On;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ TC iOS Working/Assets/Scripts/PauseMusic.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMusic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		PlayMusic.Instance.gameObject.GetComponent<AudioSource> ().Pause ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+}
