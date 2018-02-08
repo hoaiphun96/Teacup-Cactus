6e4f9f8d223e813406efa8efe16e0abd46ba8695
James Gene Woolley <jgwoolley@knox.edu>
2018-02-08 08:40:18 -0600

    Plant Crossbreeding and Inventory Update 1
    The features aren't complete, but the baseline is here for us to take a look at.

---
+++ TC iOS Working/Assets/James Temp/Flower.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flower{
+    private string[] dna;
+    private FlowerDatabase data;
+
+    private Color color;
+    private int petalCount;
+    private GameObject petalShape;
+
+    public Flower(Flower mother, Flower father, FlowerDatabase data)
+    {
+        this.data = data;
+        dna = data.Breed(mother.GetDNA(), father.GetDNA());
+    }
+
+    public Flower(string[] dna, FlowerDatabase data)
+    {
+        this.data = data;
+        this.dna = dna;
+    }
+
+    public string[] GetDNA()
+    {
+        return dna;
+    }
+}
---
+++ TC iOS Working/Assets/James Temp/Inventory.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+    private FlowerDatabase data;
+    private List<Flower> flowers;
+
+    // Use this for initialization
+    void Start () {
+        data = new TeacupCactusDatabase();
+        flowers = new List<Flower>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public class TeacupCactusDatabase : FlowerDatabase
+    {
+        public TeacupCactusDatabase() : base()
+        {
+            this.AddTraitType("PetalShape", new string[] { "pointed", "forked", "round" }, new string[] { });
+            this.AddTraitType("PetalCount", new string[] { "2", "3", "4", "5" }, new string[] { });
+            this.AddTraitType("PetalColor", new string[] { "Red", "Yellow", "Blue" }, new string[] { "Orange", "Purple", "Green" });
+            this.AddMutation("PetalColor", "Red", "Yellow", "Orange", 20);
+            this.AddMutation("PetalColor", "Red", "Blue", "Purple", 20);
+            this.AddMutation("PetalColor", "Yellow", "Blue", "Green", 20);
+        }
+    }
+
+    /*
+    Methods used for interacting with the flowers List
+    */
+
+    public void RemoveAt(int index)
+    {
+        flowers.RemoveAt(index);
+    }
+
+    public Flower Get()
+    {
+
+    }
+
+    public void Add(Flower flower)
+    {
+        flowers.Add(flower);
+    }
+
+    public int GetFlowerCount()
+    {
+        return flowers.Count;
+    }
+
+
+}
